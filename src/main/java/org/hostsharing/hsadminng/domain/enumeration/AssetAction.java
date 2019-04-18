c6be30895e9501212e07f76e353f6c9f4859d82f
Michael Hierweck <michael@hierweck.de>
2019-04-18 16:18:26 +0200

    Customer JDL

--- src/main/java/org/hostsharing/hsadminng/domain/enumeration/AssetAction.java
+++
@@ -1,8 +0,0 @@
-package org.hostsharing.hsadminng.domain.enumeration;
-
-/**
- * The AssetAction enumeration.
- */
-public enum AssetAction {
-    PAYMENT, HANDOVER, ADOPTION, LOSS, CLEARING, PAYBACK
-}
--- src/main/java/org/hostsharing/hsadminng/domain/enumeration/ShareAction.java
+++
@@ -1,8 +0,0 @@
-package org.hostsharing.hsadminng.domain.enumeration;
-
-/**
- * The ShareAction enumeration.
- */
-public enum ShareAction {
-    SUBSCRIPTION, CANCELLATION
-}
