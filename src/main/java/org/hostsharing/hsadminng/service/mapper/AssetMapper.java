c8bb8cb2ca97386a913494f7d24fbd1d5247952a
Michael Hoennig <michael@hoennig.de>
2019-04-26 17:15:39 +0200

    @AccessFrom in AssetDTO and AssetMapper with improved membershipDisplayReference

--- src/main/java/org/hostsharing/hsadminng/service/mapper/AssetMapper.java
+++ src/main/java/org/hostsharing/hsadminng/service/mapper/AssetMapper.java
@@ -1,9 +1,15 @@
 package org.hostsharing.hsadminng.service.mapper;
 
 import org.hostsharing.hsadminng.domain.Asset;
+import org.hostsharing.hsadminng.domain.Customer;
+import org.hostsharing.hsadminng.domain.Membership;
 import org.hostsharing.hsadminng.service.dto.AssetDTO;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import java.util.Objects;
 
 /**
  * Mapper for the entity Asset and its DTO AssetDTO.
@@ -12,8 +18,20 @@ import org.mapstruct.Mapping;
 public interface AssetMapper extends EntityMapper<AssetDTO, Asset> {
 
     @Mapping(source = "membership.id", target = "membershipId")
-    @Mapping(source = "membership.admissionDocumentDate", target = "membershipDisplayReference")
+    @Mapping(target = "membershipDisplayReference", ignore = true)
     AssetDTO toDto(Asset asset);
+
+    @AfterMapping
+    default void setMembershipDisplayReference(final @MappingTarget AssetDTO dto, final Asset entity) {
+        // TODO: rather use method extracted from MembershipMaper.setMembershipDisplayReference() to avoid duplicate code
+        final Membership membership = entity.getMembership();
+        final Customer customer = membership.getCustomer();
+        dto.setMembershipDisplayReference(customer.getReference()
+            + ":" + customer.getPrefix()
+            + " [" + customer.getName() + "] "
+            + membership.getAdmissionDocumentDate().toString() + " - "
+            + Objects.toString(membership.getCancellationDocumentDate(), ""));
+    }
 
     @Mapping(source = "membershipId", target = "membership")
     Asset toEntity(AssetDTO assetDTO);
