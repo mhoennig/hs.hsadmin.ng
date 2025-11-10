59901791e38ed04d81a8187f929e55655a407fce
Peter Hormanns <peter.hormanns@hostsharing.net>
2025-11-10 10:36:24 +0100

    enable cors handling (#206)
    Meine Änderung für CORS. Es gibt eine neue Environment Variable ALLOWED_ORIGINS
    
    Co-authored-by: Peter Hormanns <peter.hormanns@jalin.de>
    Reviewed-on: https://dev.hostsharing.net/hostsharing/hs.hsadmin.ng/pulls/206
    Reviewed-by: Timotheus Pokorra <timotheus.pokorra@hostsharing.net>
    Co-authored-by: Peter Hormanns <peter.hormanns@hostsharing.net>
    Co-committed-by: Peter Hormanns <peter.hormanns@hostsharing.net>

--- src/main/java/net/hostsharing/hsadminng/HsadminNgApplication.java
+++ src/main/java/net/hostsharing/hsadminng/HsadminNgApplication.java
@@ -3,6 +3,9 @@ package net.hostsharing.hsadminng;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @OpenAPIDefinition
@@ -11,4 +14,20 @@ public class HsadminNgApplication {
     public static void main(String[] args) {
         SpringApplication.run(HsadminNgApplication.class, args);
     }
+
+    @Bean
+        public WebMvcConfigurer corsConfigurer() {
+           return new WebMvcConfigurer() {
+
+               @Override
+               public void addCorsMappings(CorsRegistry registry) {
+                   String allowedOrigins = System.getenv("ALLOWED_ORIGINS");
+                   if (allowedOrigins == null || allowedOrigins.length() <= 1) {
+                       allowedOrigins = "*";
+                   }
+                   registry.addMapping("/api/**").allowedOrigins(allowedOrigins);
+               }
+           };
+       }
+
 }
